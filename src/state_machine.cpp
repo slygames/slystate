a25599369e1a52027fc620e4660dc7efec920912
slygames <238937929+slygames@users.noreply.github.com>
2026-01-22 23:43:04 +1300

    added virtual function state_changed() to state_machine

--- src/state_machine.cpp
+++ src/state_machine.cpp
@@ -7,7 +7,15 @@ void StateMachine::_bind_methods() {
     ClassDB::bind_method(D_METHOD("set_state", "p_state"), &StateMachine::set_state);
 	ClassDB::bind_method(D_METHOD("get_state"), &StateMachine::get_state);
 
+    ClassDB::bind_method(D_METHOD("set_state_past", "p_state"), &StateMachine::set_state_past);
+	ClassDB::bind_method(D_METHOD("get_state_past"), &StateMachine::get_state_past);
+
     ClassDB::bind_method(D_METHOD("get_state_name"), &StateMachine::get_state_name);
+    
+	ClassDB::bind_method(D_METHOD("state_changed"), &StateMachine::state_changed);
+    GDVIRTUAL_BIND(_state_changed);
+
+    //ADD_SIGNAL(godot::MethodInfo("on_state_changed"));
 
     /*
     // setters and getters
@@ -38,7 +46,14 @@ void StateMachine::_initialize() {
     for(int i=0; i<get_children().size();i++) {
         if(State* new_state = cast_to<State>(get_children()[i])) {
             if(new_state->is_inside_tree()) {
+                // add new state to states array
                 states[new_state->get_name()] = new_state->get_path(); // if(new_state->is_inside_tree()) else NodePath()
+
+                /*
+                // connect signals
+                new_state->connect("on_state_changed", Callable(this, "state_changed"))
+                */
+
                 // set default state to first state
                 if(i==0) {
                     set_state(new_state);
@@ -51,8 +66,15 @@ void StateMachine::_initialize() {
 
 /* Use this instead of state= because it  triggers the state exit and enter events*/
 void StateMachine::set_state(State *p_state) {
-    state_prev = state;
+    state_past = state;
     state = p_state; 
-    if(state_prev !=nullptr) state_prev->on_exit_state();
+    if(state_past !=nullptr) state_past->on_exit_state();
     if(state!=nullptr) state->on_enter_state();
+    state_changed();
+}
+
+void StateMachine::state_changed() {
+    if(GDVIRTUAL_CALL(_state_changed)) {
+		print("_state_changed gdvirtual called");
+	}
 }
--- src/state_machine.h
+++ src/state_machine.h
@@ -11,7 +11,7 @@ class StateMachine : public Node {
 	GDCLASS(StateMachine, Node)
 
 	State* state = nullptr;	// currently active state
-	State* state_prev = nullptr; // cached previous state //todo: maybe useful or remove if not useful
+	State* state_past = nullptr; // cached past state //todo: maybe useful or remove if not useful
 
 	TypedDictionary<StringName, NodePath> states;
 
@@ -29,7 +29,14 @@ public:
 	State* get_state() const { return state; }
 	void set_state(State* p_state);
 
+	State* get_state_past() const { return state_past; }
+	void set_state_past(State* p_state) { state_past = p_state; };
+
 	StringName get_state_name() { return state!=nullptr ? state->get_name().to_lower() : ""; }
+	StringName get_state_name_past() { return state_past!=nullptr ? state_past->get_name().to_lower() : ""; }
+
+	virtual void state_changed();
+	GDVIRTUAL0(_state_changed);
 
 	/*
 	// event handlers
@@ -41,4 +48,6 @@ public:
 	TypedArray<NodePath> get_states() const { return states; }
 	void set_states(TypedArray<NodePath> p_states) { states = p_states; }
 	*/
+
+	
 };
